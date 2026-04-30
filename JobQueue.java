0d56f85778169d94ea2ba1ab8afc2131bef9abf2
Maryomm <maryam78541@gmail.com>
2026-04-30 10:17:42 +0300

    Implement JobQueue class for managing process jobs

---
+++ JobQueue.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+	public class JobQueue {
+
+	    private Queue<PCB> queue;
+
+	    public JobQueue() {
+	        queue = new LinkedList<>();
+	    }
+
+	    // Add process
+	    public synchronized void addJob(PCB process) {
+	        queue.add(process);
+	        System.out.println(process + " added to Job Queue");
+	    }
+
+	    // Remove process
+	    public synchronized PCB removeJob() {
+	        return queue.poll();
+	    }
+
+	    // Check empty
+	    public synchronized boolean isEmpty() {
+	        return queue.isEmpty();
+	    }
+
+	    // Queue size
+	    public synchronized int size() {
+	        return queue.size();
+	    }
+	}
