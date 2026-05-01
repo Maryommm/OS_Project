d9f5bff7f02d8b2e94f9bf2ffc102b3aadb95bb6
wajdalrasheed-creator <wajdalrasheed@gmail.com>
2026-05-01 12:57:46 +0300

    Create MemoryManager.java

---
+++ MemoryManager.java
@@ -0,0 +1,28 @@
+
+public class MemoryManager {
+
+    private static final int TOTAL_MEMORY = 2048;
+    private int availableMemory;
+
+    public MemoryManager() {
+        this.availableMemory = TOTAL_MEMORY;
+    }
+
+    // Allocate memory
+    public synchronized boolean allocate(int memoryRequired) {
+        if (memoryRequired <= availableMemory) {
+            availableMemory -= memoryRequired;
+            return true;
+        }
+        return false;
+    }
+
+    // Release memory
+    public synchronized void release(int memoryRequired) {
+        availableMemory = Math.min(availableMemory + memoryRequired, TOTAL_MEMORY);
+    }
+
+    public synchronized int getAvailableMemory() {
+        return availableMemory;
+    }
+}
