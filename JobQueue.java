1fafc8f30e447c1c737cf204d8f1ce6f9748db33
wajdalrasheed-creator <wajdalrasheed@gmail.com>
2026-05-01 12:55:34 +0300

    Update JobQueue.java

--- JobQueue.java
+++ JobQueue.java
@@ -1,32 +1,39 @@
+package os;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
-	public class JobQueue {
+public class JobQueue {
 
-	    private Queue<PCB> queue;
+    private Queue<PCB> queue;
 
-	    public JobQueue() {
-	        queue = new LinkedList<>();
-	    }
+    public JobQueue() {
+        queue = new LinkedList<>();
+    }
+    
+    // Add process
+    public synchronized void addJob(PCB process) {
+        queue.add(process);
+        System.out.println(process + " added to Job Queue");
+    }
 
-	    // Add process
-	    public synchronized void addJob(PCB process) {
-	        queue.add(process);
-	        System.out.println(process + " added to Job Queue");
-	    }
+    // Remove process
+    public synchronized PCB removeJob() {
+        return queue.poll();
+    }
 
-	    // Remove process
-	    public synchronized PCB removeJob() {
-	        return queue.poll();
-	    }
+     // Check without removing
+    public synchronized PCB peekJob() {
+        return queue.peek();
+    }
 
-	    // Check empty
-	    public synchronized boolean isEmpty() {
-	        return queue.isEmpty();
-	    }
+    // Check empty
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
 
-	    // Queue size
-	    public synchronized int size() {
-	        return queue.size();
-	    }
-	}
+    // Queue size
+    public synchronized int size() {
+        return queue.size();
+    }
+}
