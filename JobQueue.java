2e8a4b4e973d70366bec6d6ec0cb38737288abb0
Maryomm <maryam78541@gmail.com>
2026-05-07 20:12:47 +0300

    Update comments for clarity in JobQueue.java

--- JobQueue.java
+++ JobQueue.java
@@ -5,32 +5,33 @@ public class JobQueue {
 
     private Queue<PCB> queue;
 
+    // make an empty queue
     public JobQueue() {
         queue = new LinkedList<>();
     }
     
-    // Add process
+    // Add a new process to w.queue
     public synchronized void addJob(PCB process) {
         queue.add(process);
         System.out.println(process + " added to Job Queue");
     }
 
-    // Remove process
+    // Take out the first process from the queue
     public synchronized PCB removeJob() {
         return queue.poll();
     }
 
-     // Check without removing
+    // Look at the first process without taking it out
     public synchronized PCB peekJob() {
         return queue.peek();
     }
 
-    // Check empty
+    // Check if queue empty
     public synchronized boolean isEmpty() {
         return queue.isEmpty();
     }
 
-    // Queue size
+    // Count how many jobs in queue
     public synchronized int size() {
         return queue.size();
     }
