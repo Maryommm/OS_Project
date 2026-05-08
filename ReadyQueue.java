459673bd69c9908424e5bcad3eb4b88768422228
wajdalrasheed-creator <wajdalrasheed@gmail.com>
2026-05-08 22:26:11 +0300

    Update ReadyQueue.java

--- ReadyQueue.java
+++ ReadyQueue.java
@@ -1,5 +1,3 @@
-package OS_Project;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,28 +9,34 @@ public class ReadyQueue {
         queue = new LinkedList<>();
     }
 
+    // add process to ready queue
     public synchronized void addProcess(PCB process) {
         process.setState("READY");
         queue.add(process);
     }
-
+    
+    // remove first process
     public synchronized PCB removeProcess() {
         return queue.poll();
     }
-
+    
+    // remove a specific process
     public synchronized boolean removeProcess(PCB process) {
         return queue.remove(process);
     }
 
+    // check if empty
     public synchronized boolean isEmpty() {
         return queue.isEmpty();
     }
-
+    
+    // queue size
     public synchronized int size() {
         return queue.size();
     }
 
+     // return a copy of queue
     public synchronized Queue<PCB> getQueueSnapshot() {
         return new LinkedList<>(queue);
     }
-}
+} 
